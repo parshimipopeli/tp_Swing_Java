620bfd6cc691fdbedf9040898083f9e4cd05d75b
parshimipopeli <philippe.lamorskipro@gmail.com>
2021-02-11 09:02:33 +0100

    add Mnemonic in JMenu and Keystroke in JMenuItem

--- src/fr/parshimipopeli/SwingJava/MenuBarSample.java
+++ src/fr/parshimipopeli/SwingJava/MenuBarSample.java
@@ -2,6 +2,8 @@ package fr.parshimipopeli.SwingJava;
 
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class MenuBarSample extends JFrame {
 
@@ -21,21 +23,37 @@ public class MenuBarSample extends JFrame {
         JMenuBar menuBar = new JMenuBar();
 
         JMenu menuFile = new JMenu("file");
+        menuFile.setMnemonic('F');//pour ajouter un racourcis avec premiere lettre du menu souligné à ouvrir avec touche alt + premiere lettre du menu
 
         JMenuItem menuNew = new JMenuItem("newFile");
+        menuNew.addActionListener(this::menuNewLister);
         menuFile.add(menuNew);
-        JMenuItem menu2 = new JMenuItem("menu2");
-        menuFile.add(menu2);
+        menuFile.addSeparator();
 
+        JMenu menuMenu = new JMenu("menu");
+        menuFile.add(menuMenu);
+        JMenuItem menu1 = new JMenuItem("menu-A");
+        menu1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, KeyEvent.CTRL_DOWN_MASK));//mettre un  accélerateur, racourci clavier pour ouvrir le JMenuItem
 
+        menuMenu.add(menu1);
+        JMenuItem menu2 = new JMenuItem("menu-B");
+        menu2.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, KeyEvent.CTRL_DOWN_MASK));//mettre un  accélerateur, racourci clavier pour ouvrir le JMenuItem
+
+        menuMenu.add(menu2);
 
         JMenu menuTest = new JMenu(("test"));
+        menuTest.setMnemonic('T');
+
 
         menuBar.add(menuFile);
         menuBar.add(menuTest);
         return menuBar;
     }
 
+    //methode permettant de faire ouvrir une boite de dialogue
+    private void menuNewLister(ActionEvent event) {
+        JOptionPane.showMessageDialog(this, "new document required");
+    }
 
     public static void main(String[] args) throws Exception {
         UIManager.setLookAndFeel(new NimbusLookAndFeel());
